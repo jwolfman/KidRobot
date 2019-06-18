51908f05d1444156853fd9cae3b6f46de863c394
jwolfman <joshua.wolfman@gmail.com>
2019-06-17 21:00:18 -0400

    Updates to methods and file system

---
+++ commands/affliction.js
@@ -0,0 +1,10 @@
+/**
+ * Created by joshwolfman on 6/17/19.
+ */
+module.exports={
+    name:'',
+    description:'',
+    execute:function(message,args){
+        var response=message.author;
+    }
+}
---
+++ commands/attitude.js
@@ -0,0 +1,3 @@
+/**
+ * Created by joshwolfman on 6/17/19.
+ */
