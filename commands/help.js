773b2f4fc55ad6f169c7418c139589e3302f4e4a
jwolfman <joshua.wolfman@gmail.com>
2019-06-18 23:17:33 -0400

    Updating help

--- commands/help.js
+++ commands/help.js
@@ -16,15 +16,7 @@ module.exports={
             data.push(commands.map(command => command.name).join(', '));
             data.push('\nYou can send \'${prefix}help [command name]\' to get info on a specific command!');
 
-            return message.author.send(data, { split: true })
-                .then(() => {
-                if (message.channel.type === 'dm') return;
-            message.reply('I\'ve sent you a DM with all my commands!');
-        })
-        .catch(error => {
-            console.error('Could not send help DM to '+message.author.tag+'.\n', error);
-        message.reply('it seems like I can\'t DM you!');
-        });
+            return message.channel.send(data, { split: true });
         }
         const name = args[0].toLowerCase();
         const command = commands.get(name) || commands.find(c => c.aliases && c.aliases.includes(name));
@@ -35,7 +27,7 @@ module.exports={
         if (command.aliases) data.push('**Aliases:** '+command.aliases.join(', '));
         if (command.description) data.push('**Description:** '+command.description);
         if (command.usage) data.push('**Usage:** '+prefix+command.name+' '+command.usage);
-        data.push('**Cooldown:** '+command.cooldown || 3+' second(s)');
+        data.push('**Cooldown:** '+command.cooldown || 0+' second(s)');
         message.channel.send(data, { split: true });
     }
 }
